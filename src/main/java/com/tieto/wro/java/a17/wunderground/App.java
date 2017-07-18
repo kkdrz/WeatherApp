b2915527905bf7e37fbf15b0dd2da8a68099717a
Konrad Drozd <konrad.drozd@tieto.com>
2017-07-18 14:21:18 +0200

    Modify wundeground model. Merge three cases of response into one (Error + CurrentObservation + Results). When Jersey makes GET, in a response only one of them will not be null.

--- src/main/java/com/tieto/wro/java/a17/wunderground/App.java
+++ src/main/java/com/tieto/wro/java/a17/wunderground/App.java
@@ -1,10 +1,24 @@
 package com.tieto.wro.java.a17.wunderground;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.tieto.wro.java.a17.wunderground.model.Response;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	//just for test
+    	Client client = Client.create();
+
+		WebResource webResource = client
+		   .resource("http://api.wunderground.com/api/b6bfc129d8a2c4ea/conditions/q/Poland/Wroclaw.xml");
+			//	.resource("http://api.wunderground.com/api/b6bfc129d8a2c4ea/conditions/q/Pila.xml");
+		ClientResponse clientResponse = webResource.accept("application/xml")
+                   .get(ClientResponse.class);
+		Response response = clientResponse.getEntity(Response.class);
+		
+		System.out.println(response.getCurrentObservation());
     }
 }
--- src/main/java/com/tieto/wro/java/a17/wunderground/client/WundergroundClient.java
+++ src/main/java/com/tieto/wro/java/a17/wunderground/client/WundergroundClient.java
@@ -1,5 +1,5 @@
 package com.tieto.wro.java.a17.wunderground.client;
 
 public class WundergroundClient {
-
+	
 }
