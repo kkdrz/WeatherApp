6c473237b76334300b12231f03c0d7e521fc35fb
Konrad Drozd <konrad.drozd@tieto.com>
2017-07-19 08:59:51 +0200

    Add WundergroundClient logic.

--- src/main/java/com/tieto/wro/java/a17/wunderground/App.java
+++ src/main/java/com/tieto/wro/java/a17/wunderground/App.java
@@ -1,24 +1,13 @@
 package com.tieto.wro.java.a17.wunderground;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.tieto.wro.java.a17.wunderground.model.Response;
+import com.tieto.wro.java.a17.wunderground.client.WundergroundClient;
+import javax.ws.rs.client.ClientBuilder;
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	//just for test
-    	Client client = Client.create();
-
-		WebResource webResource = client
-		   .resource("http://api.wunderground.com/api/b6bfc129d8a2c4ea/conditions/q/Poland/Wroclaw.xml");
-			//	.resource("http://api.wunderground.com/api/b6bfc129d8a2c4ea/conditions/q/Pila.xml");
-		ClientResponse clientResponse = webResource.accept("application/xml")
-                   .get(ClientResponse.class);
-		Response response = clientResponse.getEntity(Response.class);
-		
-		System.out.println(response.getCurrentObservation());
+        WundergroundClient client = new WundergroundClient(ClientBuilder.newClient());
+        client.getWeather("Poland", "Pila");
     }
 }
--- src/main/java/com/tieto/wro/java/a17/wunderground/client/WundergroundClient.java
+++ src/main/java/com/tieto/wro/java/a17/wunderground/client/WundergroundClient.java
@@ -1,5 +1,34 @@
 package com.tieto.wro.java.a17.wunderground.client;
 
+import com.tieto.wro.java.a17.wunderground.model.Response;
+import java.util.StringJoiner;
+import javax.ws.rs.client.Client;
+
 public class WundergroundClient {
-	
+
+    private static final String API_KEY = "b6bfc129d8a2c4ea";
+    private static final String API_URL = "http://api.wunderground.com/api/" + API_KEY + "/conditions/q/";
+
+    private final Client client;
+
+    public WundergroundClient(Client client) {
+        this.client = client;
+    }
+
+    public Response getWeather(String country, String city) {
+        return getResponse(buildURL(country, city));
+    }
+
+    private Response getResponse(String URL) {
+        return client.target(URL)
+                .request()
+                .get(Response.class);
+    }
+
+    private String buildURL(String country, String city) {
+        StringJoiner url = new StringJoiner("/", API_URL, ".xml");
+        url.add(country).add(city);
+        return url.toString();
+    }
+
 }
