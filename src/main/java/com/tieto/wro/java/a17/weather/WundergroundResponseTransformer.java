2aca9eeb0ba341f88878d2168f94ada254c0cf15
Konrad Drozd <konrad.drozd@tieto.com>
2017-07-24 14:06:54 +0200

    Add WundergroundResponseTransformer logging; Add log4j.

--- src/main/java/com/tieto/wro/java/a17/weather/WundergroundResponseTransformer.java
+++ src/main/java/com/tieto/wro/java/a17/weather/WundergroundResponseTransformer.java
@@ -2,11 +2,15 @@ package com.tieto.wro.java.a17.weather;
 
 import com.tieto.wro.java.a17.weather.model.CityWeather;
 import com.tieto.wro.java.a17.wunderground.model.Response;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public class WundergroundResponseTransformer {
 
     public CityWeather transform(Response response) {
+        log.info("Transforming Response to CityWeather");
         if (response == null || response.getCurrentObservation() == null) {
+            log.warn("Response is null or doesn't provide required informations.");
             return null;
         }
         Response.CurrentObservation currObs = response.getCurrentObservation();
@@ -19,6 +23,7 @@ public class WundergroundResponseTransformer {
                 currObs.getWeather(),
                 currObs.getWindString(),
                 currObs.getObservationTime());
+        log.info("Transforming Response to CityWeather completed.");
         return cw;
     }
 }
