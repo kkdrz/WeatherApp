fd5e0b920201659041c5ba1cc02e138dffbdc989
Konrad Drozd <konrad.drozd@tieto.com>
2017-09-04 12:10:11 +0200

    Add Config for Wunderground.

--- nioapp/src/main/java/com/tieto/wro/java/a17/nioapp/Config.java
+++ nioapp/src/main/java/com/tieto/wro/java/a17/nioapp/Config.java
@@ -3,6 +3,7 @@ package com.tieto.wro.java.a17.nioapp;
 
 public class Config {
 	
+//	VERTX controller config
 	public static final String CITIES_FILE = "cities.file";
 	public static final String HTTP_PORT = "http.port";
 	public static final int DEFAULT_HTTP_PORT = 8080;
@@ -11,9 +12,13 @@ public class Config {
 	public static final String SERVICE_ADDRESS = "weather.service";
 	public static final String CLIENT_ADDRESS = "wunder.client";
 	
-//	API config
+//	API config - mock
 	public static final String API_HOST = "localhost";
 	public static final int API_PORT = 8089;
 	public static final String API_PATH = "/api.wunderground.com/api/b6bfc129d8a2c4ea/conditions/q/zmw:%s.xml";
 
+// WUNDERGROUND API config
+//	public static final String API_HOST = "api.wunderground.com";
+//	public static final int API_PORT = 80;
+//	public static final String API_PATH = "/api/b6bfc129d8a2c4ea/conditions/q/zmw:%s.xml";
 }
